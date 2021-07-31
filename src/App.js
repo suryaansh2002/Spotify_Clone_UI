b43bbcfed81f63f3e61369eed06b8844023610c8
suryaansh2002 <suryaansh2002@gmail.com>
2021-08-01 00:16:29 +0530

    Added UI

--- src/App.js
+++ src/App.js
@@ -2,27 +2,88 @@ import "./App.css";
 import React, { Component } from "react";
 import Header from "./components/header.jsx";
 import Song from "./components/song.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { headersong: "Header Song", headerduration: "Song Duration" };
+    this.state = { headersong: "Hollywood's Bleeding", headerduration: "2:36" };
     this.handler = this.handler.bind(this);
     this.items = [
       {
         id: 1,
-        title: "name 1",
-        duration: "3:00",
+        title: "Hollywood's Bleeding",
+        duration: "2:36",
       },
       {
         id: 2,
-        title: "name 2",
-        duration: "3:20",
+        title: "Saint-Tropez",
+        duration: "2:30",
       },
       {
         id: 3,
-        title: "name 3",
-        duration: "4:00",
+        title: "Enemies (feat DaBaby)",
+        duration: "3:16",
+      },
+      {
+        id:4,
+        title:"Allergic",
+        duration:"2:36",
+      },
+      {
+        id:5,
+        title:"A Thousand Bad Times",
+        duration:"3:41",
+      },{
+        id:6,
+        title:"Circles",
+        duration:"3:35",
+      },{
+        id:7,
+        title:"Die For Me",
+        duration:"4:05",
+      },{
+        id:8,
+        title:"On The Road",
+        duration:"3:38",
+      },{
+        id:9,
+        title:"Take What You Want",
+        duration:"3:49",
+      },{
+        id:10,
+        title:"I'm Gonna Be",
+        duration:"3:20",
+      },
+      {
+        id:11,
+        title:"Staring At The Sun",
+        duration:"2:48",
+      },{
+        id:12,
+        title:"Sunflower: Spiderman, Into The Spiderverse",
+        duration:"2:37",
+      },{
+        id:13,
+        title:"Internet",
+        duration:"2:03",
+      },{
+        id:14,
+        title:"Goodbyes",
+        duration:"2:54",
+      },{
+        id:15,
+        title:"Myself",
+        duration:"2:38",
+      },{
+        id:16,
+        title:"I Know",
+        duration:"2:21",
+      },{
+        id:17,
+        title:"Wow",
+        duration:"2:29",
       },
     ];
   }
@@ -36,11 +97,26 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Header
+      <div className="main">
+        <Header 
           songname={this.state.headersong}
           songduration={this.state.headerduration}
         />
+        <div className="row mid">
+          <div className="col-lg-1">
+              #
+          </div>
+          <div className="col-lg-6">
+              TITLE
+          </div>
+          <div className="col-lg-3">
+          Duration
+          </div>
+
+          <div>
+
+          </div>
+        </div>
 
         {this.items.map((item) => (
           <Song
@@ -49,6 +125,7 @@ class App extends Component {
             title={item.title}
             duration={item.duration}
             onClick={() => this.handler(item)}
+            cname={item.title===this.state.headersong ? "song2": "song1"}
           />
         ))}
       </div>
--- src/components/song.jsx
+++ src/components/song.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import play from '../play-icon.png'
 
 // This is a class component
 export default class Song extends Component {
     render() {
         return (
-            <div>
-                Id: {this.props.id}; Song name: {this.props.title}; Song Duration: {this.props.duration}<br></br>
-                <button onClick={this.props.onClick}>Play</button>
-                <br></br><br></br>
-            </div>
+    <div className={`row song ${this.props.cname}`}>
+
+        <div className="col-lg-1">
+        {this.props.id}
+          </div>
+          <div className="col-lg-6">
+          <a onClick={this.props.onClick}>
+          {this.props.title}
+          </a>
+          </div>
+          <div className="col-lg-3">
+          {this.props.duration}
+          </div>
+          <div className="col-lg-2">
+          <button onClick={this.props.onClick} className="temp2">
+          <img className="play2" src={play}></img>
+          </button>
+
+          </div>               
+    </div>
         )
     }
 }
